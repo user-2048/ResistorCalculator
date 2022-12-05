c9902cf4d6975496fa67c2f7f201e9c77627444d
Karolina Mungai <93986339+user-2048@users.noreply.github.com>
2022-12-05 16:13:05 +0000

    Added comments for each section of planned code. No code implemented yet.

--- .ccls-cache/@home@runner@ResistorCalculator/main.cpp
+++ .ccls-cache/@home@runner@ResistorCalculator/main.cpp
@@ -1,5 +1,43 @@
+/*********************************************************/
+/* Resistor Calculator - Karolina Mungai Ndungu          */
+/* Module: ELEC2645 Embedded Systems.                    */
+/*                                                       */
+/* This menu item will calculate the value of a 4-band   */
+/* resistor, when the user inputs the 4 colours.         */
+/*********************************************************/
+
+// include relevant libraries
 #include <iostream>
 
+// and header files
+
+
+// main function:
+// 1. a) initialise colour1, colour2, colour3, colour4.
+//    b) user input
+//    c) error checking. check that the colours are valid.
+//    d) calculate resistor value
+//    e) output the calculated value
 int main() {
   std::cout << "Hello World" << std::endl;
 }
+
+// 1a. initialisation functions: initialises each band to be a map structure.
+//     The colour chooses the key to keep. Maybe delete the rest?
+// initc1()
+// initc2()
+// initc3()
+// initc4()
+
+// 1b. user input
+// get user input()
+
+// 1c. error handling - done for each band (1 and 2 are the same)
+// i - errorc1c2()
+// ii - errorc3()
+// iii- errorc4()
+
+// 1d. calculate the resistor value.
+// resistorValue()
+
+// 1e. output resistor value.
