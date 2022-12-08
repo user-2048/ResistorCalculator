95a3e9aa9d34755d25dda8f925425cee0a67cd7d
Karolina Mungai <93986339+user-2048@users.noreply.github.com>
2022-12-08 23:34:09 +0000

    Implemented all the code, need to add functions.

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ void initc1();
 //    f) output the calculated value
 int main() {
   // a) print all the colours in the four bands
-    cout << "Band 1: black, brown, red, orange, yellow, green, blue, violet, "
+  cout << "Band 1: black, brown, red, orange, yellow, green, blue, violet, "
           "grey, white\n";
   cout << "Band 2: black, brown, red, orange, yellow, green, blue, violet, "
           "grey, white\n";
@@ -60,33 +60,29 @@ int main() {
   colour2["grey"] = '8';
   colour2["white"] = '9';
   
-  map<string, string> colour3; // multiplier
-  colour3["black"] = '0';
-  colour3["brown"] = '1';
-  colour3["red"] = '2';
-  colour3["orange"] = '3';
-  colour3["yellow"] = '4';
-  colour3["green"] = '5';
-  colour3["blue"] = '6';
-  colour3["violet"] = '7';
-  colour3["grey"] = '8';
-  colour3["white"] = '9';
-  colour3["gold"] = '9';
-  colour3["silver"] = '10';
+  map<string, double> colour3; // multiplier
+  colour3["black"] = 1;
+  colour3["brown"] = 10;
+  colour3["red"] = 100;
+  colour3["orange"] = 1000; // 1kOhm
+  colour3["yellow"] = 10000; // 10k
+  colour3["green"] = 100000; // 100k
+  colour3["blue"] = 1000000; // 1MOhm
+  colour3["violet"] = 10000000; // 10M
+  colour3["grey"] = 100000000; // 100M
+  colour3["white"] = 1000000000; // 1GOHm
+  colour3["gold"] = 0.1;
+  colour3["silver"] = 0.01;
   
-  map<string, string> colour4; // tolerance
-  colour4["black"] = '0';
-  colour4["brown"] = '1';
-  colour4["red"] = '2';
-  colour4["orange"] = '3';
-  colour4["yellow"] = '4';
-  colour4["green"] = '5';
-  colour4["blue"] = '6';
-  colour4["violet"] = '7';
-  colour4["grey"] = '8';
-  colour4["gold"] = '9';
-  colour4["silver"] = '11';
-  
+  map<string, double> colour4; // tolerance (these are all +/- the percentage)
+  colour4["red"] = 2;
+  colour4["brown"] = 1;
+  colour4["green"] = 0.5;
+  colour4["blue"] = 0.25;
+  colour4["violet"] = 0.1;
+  colour4["grey"] = 0.05;
+  colour4["gold"] = 5;
+  colour4["silver"] = 10;
 
   // c) user input
   string c1, c2, c3, c4;
@@ -94,36 +90,34 @@ int main() {
   cin >> c1 >> c2 >> c3 >> c4;
 
   // d) error checking. check that the colours are valid.
-    if (colour1.count(c1) == 0) { // band 1
+  if (colour1.count(c1) == 0) { // band 1
     cout << c1 << " is an invalid colour.\n";
     return (1);
-  } else {
-    cout << c1 << " does exist and has a value of " << colour1[c1] << endl;
   }
 
   if (colour2.count(c2) == 0) { // band 2
     cout << c2 << " is an invalid colour.\n";
     return (1);
-  } else {
-    cout << c2 << " does exist and has a value of " << colour2[c2] << endl;
   }
 
   if (colour3.count(c3) == 0) { // band 3
     cout << c3 << " is an invalid colour.\n";
     return (1);
-  } else {
-    cout << c3 << " does exist and has a value of " << colour3[c3] << endl;
   }
 
   if (colour4.count(c4) == 0) { // band 4
     cout << c4 << " is an invalid colour.\n";
     return (1);
-  } else {
-    cout << c4 << " does exist and has a value of " << colour4[c4] << endl;
   }
   
   // e) calculate resistor value
+  string c1c2Val = colour1[c1] + colour2[c2];
+  double rVal = stod(c1c2Val);
+
+  rVal = rVal*colour3[c3];
+  
   // f) output the calculated value
+  cout << "Calculated resistor value = " << rVal << ", +/-" << colour4[c4] << "%\n"; 
 
   return (0);
 }
